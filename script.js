b1b367db2f931083e8cab788c3bafeb03d9996f8
Md Rokan Uddin <rokanuddinroman@gmail.com>
2022-03-22 03:43:38 +0600

    test

--- script.js
+++ script.js
@@ -25,3 +25,26 @@ function scrollHeader() {
     if (this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+/*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) => {
+    const openBtn = document.getElementById(openButton),
+        modalContainer = document.getElementById(modalContent)
+
+    if (openBtn && modalContainer) {
+        openBtn.addEventListener('click', () => {
+            modalContainer.classList.add('show-modal')
+        })
+    }
+}
+showModal('open-modal', 'modal-container')
+
+/*=============== CLOSE MODAL ===============*/
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal() {
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.classList.remove('show-modal')
+}
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
